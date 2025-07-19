2ed86f81d2e002cb623ce83a13eaf4550d3e4265
bakersss93 <47493024+bakersss93@users.noreply.github.com>
2025-07-19 20:33:41 +0930

    Update Synergy API URL default

--- config/synergy.php
+++ config/synergy.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
-    'api_url' => env('SYNERGY_API_URL', 'https://api.synergywholesale.com'),
+    // Full WSDL endpoint for the Synergy Wholesale API
+    'api_url' => env('SYNERGY_API_URL', 'https://api.synergywholesale.com/server.php?wsdl'),
+
     'reseller_id' => env('SYNERGY_RESELLER_ID', ''),
     'api_key' => env('SYNERGY_API_KEY', ''),
 ];
