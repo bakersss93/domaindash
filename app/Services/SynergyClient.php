537980d830f2607d219c8f75466296d798cc1f79
bakersss93 <47493024+bakersss93@users.noreply.github.com>
2025-07-20 09:50:18 +0930

    Inject Synergy client and use bulkDomainInfo (#5)

---
+++ app/Services/SynergyClient.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services;
+
+class SynergyClient
+{
+    private \SoapClient $client;
+    private string $resellerId;
+    private string $apiKey;
+
+    public function __construct()
+    {
+        $this->resellerId = config('synergy.reseller_id');
+        $this->apiKey = config('synergy.api_key');
+
+        $this->client = new \SoapClient(config('synergy.api_url'), [
+            'trace' => 1,
+            'exceptions' => true,
+            'cache_wsdl' => WSDL_CACHE_NONE,
+        ]);
+    }
+
+    private function request(string $method, array $params = [])
+    {
+        $params = array_merge([
+            'resellerID' => $this->resellerId,
+            'apiKey' => $this->apiKey,
+        ], $params);
+
+        try {
+            $response = $this->client->__soapCall($method, [$params]);
+            return json_decode(json_encode($response), true);
+        } catch (\SoapFault $e) {
+            throw new \Exception("SOAP API Error: {$e->getMessage()}");
+        }
+    }
+
+    public function bulkDomainInfo(array $params)
+    {
+        return $this->request('BulkDomainInfo', $params);
+    }
+
+    public function __call(string $name, array $arguments)
+    {
+        $params = $arguments[0] ?? [];
+        return $this->request($name, $params);
+    }
+}
